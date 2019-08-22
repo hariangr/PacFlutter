f4de93cf3074266ebccc0432b8fa18aafeec2fa3
Hari Anugrah <code.anugrah@gmail.com>
2019-08-22 16:46:20 +0800

    idk

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:web_socket_channel/io.dart';
+import 'dart:convert';
+import 'package:web_socket_channel/status.dart' as status;
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -45,52 +35,78 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  IOWebSocketChannel channel;
+  CameraController controller;
+  List<CameraDescription> cameras;
+  int curCamera = 0;
+  Directory appDir;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    channel = IOWebSocketChannel.connect("ws://192.168.1.6:3000");
+    loadCameras();
+    // channel = IOWebSocketChannel.connect("ws://demos.kaazing.com/echo");
+    channel.stream.listen((data) {
+      if (data == "cheese") {
+        print('suuus');
+        onShutterPressed();
+      }
+    });
+  }
+
+  void onShutterPressed() async {
+    print('menyimpan');
+
+    String date = DateTime.now().millisecondsSinceEpoch.toString();
+    String tempDir = (await getTemporaryDirectory()).path;
+
+    var path = '$tempDir/$date.jpg';
+    await controller.takePicture(path);
+    File f = File(path);
+    String encoded = base64Encode(f.readAsBytesSync());
+
+    String fileName = Random.secure().nextInt(20).toString();
+    channel.sink.add(json.encode({"event": "save", "data": {
+      "filename": 'file$fileName',
+      "encoded": encoded,
+    }}));
+
+    print(path);
+  }
+
+  Future<void> loadCameras() async {
+    cameras = await availableCameras();
+    loadCameraController();
+  }
+
+  void loadCameraController() {
+    // Untuk pilih kamera
+    var camIndex = curCamera % cameras.length;
+
+    controller = CameraController(cameras[camIndex], ResolutionPreset.low);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: CameraPreview(controller)),
             Text(
               'You have pushed the button this many times:',
             ),
@@ -98,14 +114,27 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            StreamBuilder(
+              stream: channel.stream,
+              initialData: "Empty",
+              builder: (context, snap) {
+                return Text(snap.data);
+              },
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          channel.sink.add(json
+              .encode({"event": "new", "data": DateTime.now().toString()}));
+
+          channel.sink.add(json
+              .encode({"event": "cheese", "data": DateTime.now().toString()}));
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
